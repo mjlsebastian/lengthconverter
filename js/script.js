6d6bdb192c1bd749596530cc126a64a5525ce32e
mj <mjsebastian27@gmail.com>
2022-11-05 19:44:21 +0800

    second commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ var inputType = document.getElementById("inputType");
 var resultType = document.getElementById("resultType");
 var inputTypeValue, resultTypeValue;
 
-input.addEventListener("keyup",myResult);
 inputType.addEventListener("change",myResult);
 resultType.addEventListener("change",myResult);
 
